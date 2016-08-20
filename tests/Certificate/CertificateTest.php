5f7c6da9538ebf2b3f404a847882d398f2487db7
Antonio Spinelli <tonicospinelli85@gmail.com>
2016-08-20 12:45:18 -0300

    add readPfx method to able flexibility

--- tests/Certificate/CertificateTest.php
+++ tests/Certificate/CertificateTest.php
@@ -7,11 +7,29 @@ use NFePHP\Common\Exception\CertificateException;
 
 class CertificateTest extends \PHPUnit_Framework_TestCase
 {
-    const TEST_CERTIFICATE_FILE = '/../fixtures/certs/certificado_teste.pfx';
+    const TEST_PFX_FILE = '/../fixtures/certs/certificado_teste.pfx';
+
+    const TEST_PRIVATE_KEY = '/../fixtures/certs/x99999090910270_priKEY.pem';
+
+    const TEST_PUBLIC_KEY = '/../fixtures/certs/x99999090910270_pubKEY.pem';
 
     public function testShouldLoadPfxCertificate()
     {
-        $certificate = new Certificate(file_get_contents(__DIR__ . self::TEST_CERTIFICATE_FILE), 'associacao');
+        $certificate = Certificate::readPfx(file_get_contents(__DIR__ . self::TEST_PFX_FILE), 'associacao');
+        $this->assertEquals('NFe - Associacao NF-e:99999090910270', $certificate->getCompanyName());
+        $this->assertEquals(new \DateTime('2009-05-22 17:07:03'), $certificate->getValidFrom());
+        $this->assertEquals(new \DateTime('2010-10-02 17:07:03'), $certificate->getValidTo());
+        $this->assertTrue($certificate->isExpired());
+        $dataSigned = $certificate->sign('nfe');
+        $this->assertTrue($certificate->verify('nfe', $dataSigned));
+    }
+
+    public function testShouldLoadCertificate()
+    {
+        $certificate = new Certificate(
+            new Certificate\PrivateKey(file_get_contents(__DIR__ . self::TEST_PRIVATE_KEY)),
+            new Certificate\PublicKey(file_get_contents(__DIR__ . self::TEST_PUBLIC_KEY))
+        );
         $this->assertInstanceOf(Certificate::class, $certificate);
         $this->assertEquals('NFe - Associacao NF-e:99999090910270', $certificate->getCompanyName());
         $this->assertEquals(new \DateTime('2009-05-22 17:07:03'), $certificate->getValidFrom());
@@ -24,6 +42,6 @@ class CertificateTest extends \PHPUnit_Framework_TestCase
     public function testShouldGetExceptionWhenLoadPfxCertificate()
     {
         $this->setExpectedException(CertificateException::class);
-        new Certificate(file_get_contents(__DIR__ . self::TEST_CERTIFICATE_FILE), 'error');
+        Certificate::readPfx(file_get_contents(__DIR__ . self::TEST_PFX_FILE), 'error');
     }
 }
