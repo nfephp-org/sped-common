0d1d4e9a0c012341e01d74efe7ed811d5dbae52d
robmachado <linux.rlm@gmail.com>
2018-04-12 18:28:43 -0300

    [UPD] ASN1 removendo notice quando certificado não possui CNPJ

--- src/Certificate/Asn1.php
+++ src/Certificate/Asn1.php
@@ -27,8 +27,27 @@ class Asn1
      */
     public static function getCNPJ($publickeyUnformated)
     {
+        //CNPJ
+        //OID = 2.16.76.1.3.3 
         return self::getOIDdata('2.16.76.1.3.3', $publickeyUnformated);
     }
+    
+    /**
+     * CPF
+     * OID = 2.16.76.1.3.1 e 
+     * conteúdo = nas primeiras 8 (oito) posições,
+     *  a data de nascimento do titular, 
+     * no formato ddmmaaaa; 
+     * nas 11 (onze) posições subseqüentes, 
+     * o Cadastro de Pessoa Física (CPF) do titular;
+     * nas 11 (onze) posições subseqüentes, 
+     * o número de inscrição do titular no PIS/PASEP; 
+     * nas 11 (onze) posições subseqüentes, 
+     * o número do Registro Geral - RG do titular; 
+     * nas 6 (seis) posições subseqüentes, 
+     * as siglas do órgão expedidor do RG 
+     * e respectiva UF.
+     */
 
     /**
      * Recovers information regarding the OID contained in the certificate
@@ -51,6 +70,10 @@ class Asn1
         //because there are usually only one OID of each type in
         //the certificate, but can be more. In this case only
         //first occurency will be returned.
+        if (!strpos($certder, $oidMarker)) {
+            //OID not found return empty
+            return '';
+        }
         $partes = explode($oidMarker, $certder);
         //if count($partes) > 1 so OID was located
         $tot = count($partes);
