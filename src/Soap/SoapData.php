8d4105e6dfabf8ccaf7ed972db627a271838673c
Zoio Silva <zoiosilva@yahoo.com.br>
2018-05-24 09:24:47 -0400

    Refatorado o método de envio, sem modificar funcionalidades. Adicionado método público de envio novo, que faz a mesma coisa, mas com parâmetros de entrada diferentes.

---
+++ src/Soap/SoapData.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NFePHP\Common\Soap;
+
+class SoapData
+{
+    public $urlService;
+    public $urlMethod;
+    public $urlAction;
+    public $soapNamespaces;
+    public $objHeader;
+    public $envelopedData;
+    public $contentType;
+}
