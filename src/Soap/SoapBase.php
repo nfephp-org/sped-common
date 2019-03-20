5aefd3c4c11af5af97b069982606495f5e57ed28
robmachado <linux.rlm@gmail.com>
2019-03-20 16:18:03 -0300

    [UPD] Adicionado CURLOPT_HTTP_VERSION para contornar problemas de má configuração de servidores onde o handshake não informa a versão correta

--- src/Soap/SoapBase.php
+++ src/Soap/SoapBase.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace NFePHP\Common\Soap;
 
 use NFePHP\Common\Certificate;
@@ -13,7 +14,7 @@ use Psr\Log\LoggerInterface;
  *
  * @category  NFePHP
  * @package   NFePHP\Common\Soap\SoapBase
- * @copyright NFePHP Copyright (c) 2017
+ * @copyright NFePHP Copyright (c) 2017-2019
  * @author    Roberto L. Machado <linux.rlm at gmail dot com>
  * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
  * @license   https://opensource.org/licenses/MIT MIT
@@ -111,6 +112,10 @@ abstract class SoapBase implements SoapInterface
      */
     protected $encriptPrivateKey = false;
     /**
+     * @var integer
+     */
+    protected $httpver;
+    /**
      * @var bool
      */
     protected $debugmode = false;
@@ -142,7 +147,7 @@ abstract class SoapBase implements SoapInterface
      * @var int
      */
     public $waitingTime = 45;
-
+    
     /**
      * SoapBase constructor.
      * @param Certificate|null $certificate
@@ -199,7 +204,29 @@ abstract class SoapBase implements SoapInterface
     {
         return $this->disableCertValidation = $flag;
     }
-
+    
+    /**
+     * Force http protocol version
+     * 
+     * @param null|string $version
+     */
+    public function httpVersion($version = null)
+    {
+        switch ($version) {
+            case '1.0':
+                $this->httpver = CURL_HTTP_VERSION_1_0;
+                break;
+            case '1.1':
+                $this->httpver = CURL_HTTP_VERSION_1_1;
+                break;
+            case '2.0':
+                $this->httpver = CURL_HTTP_VERSION_2_0;
+                break;
+            default:
+                $this->httpver = CURL_HTTP_VERSION_NONE;
+        }
+    }
+    
     /**
      * Load path to CA and enable to use on SOAP
      * @param string $capath
