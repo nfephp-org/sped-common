db83ea6e23afb83f58ed34984b780c9e78d8e7fb
Gerson Felipe Schwinn <gerson@onehost.com.br>
2024-07-01 18:43:49 -0300

    Melhorando testes

--- tests/SignerMultiTest.php
+++ tests/SignerMultiTest.php
@@ -13,10 +13,19 @@ class SignerMultiTest extends TestCase
         $pfx = file_get_contents(__DIR__ . '/fixtures/certs/certificado_teste.pfx');
         $certificate = Certificate::readPfx($pfx, 'associacao');
 
-        $xmlsign = SignerMulti::sign($certificate, '<a><b><c>Teste Assinar mesmo documento 2x</c></b></a>', 'b', 'Id', OPENSSL_ALGO_SHA1, SignerMulti::CANONICAL, 'b');
-        $xmlsign2x = SignerMulti::sign($certificate, $xmlsign, 'b', 'Id', OPENSSL_ALGO_SHA1, SignerMulti::CANONICAL, 'a');
+        $xml = '<a>';
+        $xml .=  '<b>';
+        $xml .=     '<c>Teste Assinar mesmo documento 2x</c>';
+        $xml .=  '</b>';
+        $xml .= '</a>';
+
+        $xmlSignTagB = SignerMulti::sign($certificate, $xml, 'b', 'Id', OPENSSL_ALGO_SHA1, SignerMulti::CANONICAL, 'b');
+        $xmlsignTagA = SignerMulti::sign($certificate, $xmlSignTagB, 'b', 'Id', OPENSSL_ALGO_SHA1, SignerMulti::CANONICAL, 'a');
+
         $dom = new \DOMDocument('1.0', 'utf-8');
-        $dom->loadXML($xmlsign2x);
+        $dom->loadXML($xmlsignTagA);
         $this->assertEquals(2, $dom->getElementsByTagName('Signature')->count());
+        $this->assertEquals('Signature', $dom->getElementsByTagName('a')->item(0)->childNodes->item(1)->nodeName);
+        $this->assertEquals('Signature', $dom->getElementsByTagName('b')->item(0)->childNodes->item(1)->nodeName);
     }
 }
