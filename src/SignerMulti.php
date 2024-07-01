db5f4523f2cd701cfb8ba5e148ba3d141aeee462
Gerson Felipe Schwinn <gerson@onehost.com.br>
2024-07-01 18:46:37 -0300

    Alterando comentário

--- src/SignerMulti.php
+++ src/SignerMulti.php
@@ -1,25 +1,7 @@
 <?php
 
 /**
- * Class to signner a Xml
- * Meets packages :
- *     sped-nfe,
- *     sped-cte,
- *     sped-mdfe,
- *     sped-nfse,
- *     sped-efinanceira
- *     sped-esocial
- *     sped-efdreinf
- *     e sped-esfinge
- *
- * @category  NFePHP
- * @package   NFePHP\Common\Signer
- * @copyright NFePHP Copyright (c) 2016
- * @license   http://www.gnu.org/licenses/lgpl.txt LGPLv3+
- * @license   https://opensource.org/licenses/MIT MIT
- * @license   http://www.gnu.org/licenses/gpl.txt GPLv3+
- * @author    Roberto L. Machado <linux.rlm at gmail dot com>
- * @link      http://github.com/nfephp-org/sped-common for the canonical source repository
+ * Class to sign multiple times the same xml
  */
 
 namespace NFePHP\Common;
